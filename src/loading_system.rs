3f64b93ee29c9771e9c368e81e12db0c35ace167
note <omeraligencoglu7@gmail.com>
2025-10-25 02:56:21 -0400

    set up loading system and separate code into modules

---
+++ src/loading_system.rs
@@ -0,0 +1,76 @@
+use super::LevelHandles;
+use super::MyAppState;
+use bevy::{asset::LoadState, asset::UntypedAssetId, prelude::*};
+
+// A list of assets currently being loaded.
+// This should be a Single resource
+// Should always be empty if app state is not Loading
+#[derive(Default, Resource)]
+pub struct AssetsLoading(Vec<UntypedHandle>);
+
+// Start loading assets for the level.
+pub fn start_loading_assets(
+    asset_server: Res<AssetServer>,
+    mut loading: ResMut<AssetsLoading>,
+    mut lvl_handles: ResMut<LevelHandles>,
+) {
+    debug!("Loading assets...");
+    let level_mesh: Handle<Mesh> = asset_server.load("models/dungeon.glb#Mesh0/Primitive0");
+    loading.0.push(level_mesh.clone().untyped());
+    lvl_handles.0.push(level_mesh.clone());
+}
+
+// get a cumulative load state for a list of handles (only success if all of them are loaded)
+// apparently this used to be a library method but was removed during a revamp and never added back
+pub fn get_group_load_state(
+    server: &AssetServer,
+    handles: impl IntoIterator<Item = UntypedAssetId>,
+) -> LoadState {
+    let mut load_state = LoadState::Loaded;
+    for handle_id in handles {
+        match server.get_load_state(handle_id) {
+            Some(LoadState::Loaded) => continue,
+            Some(LoadState::Loading) => {
+                load_state = LoadState::Loading;
+            }
+            Some(LoadState::Failed(x)) => return LoadState::Failed(x),
+            Some(LoadState::NotLoaded) => return LoadState::NotLoaded,
+            None => return LoadState::NotLoaded,
+        }
+    }
+
+    load_state
+}
+
+// Check if the assets currently being loaded are done loading.
+// This runs every frame if app state is Loading, and will transition app state to InGame
+// if all assets are loaded.
+// todo: find a way to do this faster
+pub fn checks_assets_loaded(
+    mut commands: Commands,
+    mut next_state: ResMut<NextState<MyAppState>>,
+    asset_server: Res<AssetServer>,
+    loading: Res<AssetsLoading>,
+) {
+    let state = get_group_load_state(&asset_server, loading.0.iter().map(|h| h.id()));
+    match state {
+        LoadState::Failed(err) => {
+            // one of our assets had an error
+            panic!("couldnt get load state of asset. {}", err);
+        }
+        LoadState::Loaded => {
+            // all assets are now ready
+
+            // this might be a good place to transition into your in-game state
+            debug!("Loaded all assets, switching to ingame state");
+            next_state.set(MyAppState::InGame);
+            // remove the resource to drop the tracking handles
+            commands.remove_resource::<AssetsLoading>();
+            // (note: if you don't have any other handles to the assets
+            // elsewhere, they will get unloaded after this)
+        }
+        _ => {
+            // NotLoaded/Loading: not fully ready yet
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
-use bevy::{
-    input::{InputSystem, mouse::MouseMotion},
-    prelude::*,
-};
+use crate::loading_system::*;
+use crate::player_movement::*;
+use bevy::{input::InputSystem, prelude::*};
 use bevy_rapier3d::{control::KinematicCharacterController, prelude::*};
 
-const MOUSE_SENSITIVITY: f32 = 0.3;
-const GROUND_TIMER: f32 = 0.5;
-const MOVEMENT_SPEED: f32 = 8.0;
-const JUMP_SPEED: f32 = 20.0;
-const GRAVITY: f32 = -9.81;
+pub mod loading_system;
+pub mod player_movement;
+
+// States of the app in general. Could become more complicated in the future
+#[derive(States, Debug, Clone, PartialEq, Eq, Hash)]
+pub enum MyAppState {
+    Loading,
+    InGame,
+}
+
+// For systems that should only run when app is loading
+#[derive(SystemSet, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct LoadingSet;
+
+// For systems that should only run when app is in game
+#[derive(SystemSet, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct GameplaySet;
+
+// a list of mesh handles for the levels currently open.
+// This should be a Single resource
+// Clear vector to unload resources whenever possible
+#[derive(Default, Resource)]
+pub struct LevelHandles(Vec<Handle<Mesh>>);
 
 fn main() {
     App::new()
@@ -19,15 +36,49 @@ fn main() {
         )))
         .init_resource::<MovementInput>()
         .init_resource::<LookInput>()
+        .init_resource::<AssetsLoading>()
+        .init_resource::<LevelHandles>()
         .add_plugins((
             DefaultPlugins,
             RapierPhysicsPlugin::<NoUserData>::default(),
             RapierDebugRenderPlugin::default(),
         ))
-        .add_systems(Startup, (setup_player, setup_map))
-        .add_systems(PreUpdate, handle_input.after(InputSystem))
-        .add_systems(Update, player_look)
-        .add_systems(FixedUpdate, player_movement)
+        .insert_state(MyAppState::Loading)
+        .configure_sets(
+            PreUpdate,
+            (
+                GameplaySet.run_if(in_state(MyAppState::InGame)),
+                LoadingSet.run_if(in_state(MyAppState::Loading)),
+            ),
+        )
+        .configure_sets(
+            Update,
+            (
+                GameplaySet.run_if(in_state(MyAppState::InGame)),
+                LoadingSet.run_if(in_state(MyAppState::Loading)),
+            ),
+        )
+        .configure_sets(
+            FixedUpdate,
+            (
+                GameplaySet.run_if(in_state(MyAppState::InGame)),
+                LoadingSet.run_if(in_state(MyAppState::Loading)),
+            ),
+        )
+        .add_systems(OnEnter(MyAppState::InGame), (spawn_level_map, setup_player))
+        .add_systems(Startup, start_loading_assets)
+        .add_systems(
+            PreUpdate,
+            ((handle_input.after(InputSystem)).in_set(GameplaySet),),
+        )
+        .add_systems(
+            Update,
+            (
+                (player_look).in_set(GameplaySet),
+                (checks_assets_loaded).in_set(LoadingSet),
+            ),
+        )
+        .add_systems(FixedUpdate, ((player_movement).in_set(GameplaySet),))
         .run();
 }
 
@@ -62,135 +113,32 @@ pub fn setup_player(mut commands: Commands) {
         });
 }
 
-fn setup_map(mut commands: Commands) {
-    /*
-     * Ground
-     */
-    let ground_size = 50.0;
-    let ground_height = 0.1;
-
-    commands.spawn((
-        Transform::from_xyz(0.0, -ground_height, 0.0),
-        Collider::cuboid(ground_size, ground_height, ground_size),
-    ));
-    /*
-     * Stairs
-     */
-    let stair_len = 30;
-    let stair_step = 0.2;
-    for i in 1..=stair_len {
-        let step = i as f32;
-        let collider = Collider::cuboid(1.0, step * stair_step, 1.0);
-        commands.spawn((
-            Transform::from_xyz(40.0, step * stair_step, step * 2.0 - 20.0),
-            collider.clone(),
-        ));
-        commands.spawn((
-            Transform::from_xyz(-40.0, step * stair_step, step * -2.0 + 20.0),
-            collider.clone(),
-        ));
-        commands.spawn((
-            Transform::from_xyz(step * 2.0 - 20.0, step * stair_step, 40.0),
-            collider.clone(),
-        ));
-        commands.spawn((
-            Transform::from_xyz(step * -2.0 + 20.0, step * stair_step, -40.0),
-            collider.clone(),
-        ));
-    }
-}
-
-/// Keyboard input vector
-#[derive(Default, Resource, Deref, DerefMut)]
-struct MovementInput(Vec3);
-
-/// Mouse input vector
-#[derive(Default, Resource, Deref, DerefMut)]
-struct LookInput(Vec2);
-
-fn handle_input(
-    keyboard: Res<ButtonInput<KeyCode>>,
-    mut movement: ResMut<MovementInput>,
-    mut look: ResMut<LookInput>,
-    mut mouse_events: EventReader<MouseMotion>,
+// todo: change this to get in a scene map
+fn spawn_level_map(
+    mut commands: Commands,
+    meshes: Res<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    lvl_handles: Res<LevelHandles>,
 ) {
-    if keyboard.pressed(KeyCode::KeyW) {
-        movement.z -= 1.0;
-    }
-    if keyboard.pressed(KeyCode::KeyS) {
-        movement.z += 1.0
-    }
-    if keyboard.pressed(KeyCode::KeyA) {
-        movement.x -= 1.0;
-    }
-    if keyboard.pressed(KeyCode::KeyD) {
-        movement.x += 1.0
-    }
-    **movement = movement.normalize_or_zero();
-    if keyboard.pressed(KeyCode::ShiftLeft) {
-        **movement *= 2.0;
-    }
-    if keyboard.pressed(KeyCode::Space) {
-        movement.y = 1.0;
-    }
+    let lvl_mesh_handle = &lvl_handles.0[0];
+    let lvl_mesh = meshes
+        .get(lvl_mesh_handle)
+        .expect("Could not find level mesh");
 
-    for event in mouse_events.read() {
-        look.x -= event.delta.x * MOUSE_SENSITIVITY;
-        look.y -= event.delta.y * MOUSE_SENSITIVITY;
-        look.y = look.y.clamp(-89.9, 89.9); // Limit pitch
-    }
-}
-
-fn player_movement(
-    time: Res<Time>,
-    mut input: ResMut<MovementInput>,
-    mut player: Query<(
-        &mut Transform,
-        &mut KinematicCharacterController,
-        Option<&KinematicCharacterControllerOutput>,
-    )>,
-    mut vertical_movement: Local<f32>,
-    mut grounded_timer: Local<f32>,
-) {
-    let Ok((transform, mut controller, output)) = player.single_mut() else {
-        return;
-    };
-    let delta_time = time.delta_secs();
-    // Retrieve input
-    let mut movement = Vec3::new(input.x, 0.0, input.z) * MOVEMENT_SPEED;
-    let jump_speed = input.y * JUMP_SPEED;
-    // Clear input
-    **input = Vec3::ZERO;
-    // Check physics ground check
-    if output.map(|o| o.grounded).unwrap_or(false) {
-        *grounded_timer = GROUND_TIMER;
-        *vertical_movement = 0.0;
-    }
-    // If we are grounded we can jump
-    if *grounded_timer > 0.0 {
-        *grounded_timer -= delta_time;
-        // If we jump we clear the grounded tolerance
-        if jump_speed > 0.0 {
-            *vertical_movement = jump_speed;
-            *grounded_timer = 0.0;
+    let lvl_collider = Collider::from_bevy_mesh(
+        lvl_mesh,
+        &ComputedColliderShape::TriMesh(TriMeshFlags::all()),
+    );
+    match lvl_collider {
+        Some(collider) => {
+            commands
+                .spawn(Mesh3d(lvl_mesh_handle.clone()))
+                .insert(MeshMaterial3d(materials.add(Color::WHITE)))
+                .insert(Name::new("Level Mesh"))
+                .insert(collider);
+        }
+        None => {
+            panic!("Could not generate collider for level mesh");
         }
     }
-    movement.y = *vertical_movement;
-    *vertical_movement += GRAVITY * delta_time * controller.custom_mass.unwrap_or(1.0);
-    controller.translation = Some(transform.rotation * (movement * delta_time));
-}
-
-fn player_look(
-    mut player: Query<&mut Transform, (With<KinematicCharacterController>, Without<Camera>)>,
-    mut camera: Query<&mut Transform, With<Camera>>,
-    input: Res<LookInput>,
-) {
-    let Ok(mut transform) = player.single_mut() else {
-        return;
-    };
-    transform.rotation = Quat::from_axis_angle(Vec3::Y, input.x.to_radians());
-    let Ok(mut transform) = camera.single_mut() else {
-        return;
-    };
-    transform.rotation = Quat::from_axis_angle(Vec3::X, input.y.to_radians());
 }
