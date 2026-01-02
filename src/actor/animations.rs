bbea65fb50518d308fbf233dcb06ffd975036887
Ada Gencoglu <adashiv.rs@gmail.com>
2026-01-02 12:32:35 -0500

    set up utils file

--- src/actor/animations.rs
+++ src/actor/animations.rs
@@ -1,5 +1,7 @@
 use std::time::Duration;
 use bevy::prelude::*;
+use crate::utils::get_top_parent;
+
 use super::SKELETON_PATH;
 
 #[derive(Resource)]
@@ -30,22 +32,7 @@ pub fn load_animations(
 
 }
 
-// next two methods built with reference from here:
-// https://github.com/SnowdenWintermute/bevy-multiple-characters-animation/blob/main/src/animated_character/link_animations.rs
-pub fn get_top_parent(
-    mut curr_entity: Entity,
-    all_entities_with_parents_query: &Query<&ChildOf>,
-) -> Entity {
-    // Loop up all the way to the top parent
-    loop {
-        if let Ok(ref_to_parent) = all_entities_with_parents_query.get(curr_entity) {
-            curr_entity = ref_to_parent.parent();
-        } else {
-            break;
-        }
-    }
-    curr_entity
-}
+
 
 #[derive(Component, Debug)]
 pub struct AnimationEntityLink(pub Entity);
